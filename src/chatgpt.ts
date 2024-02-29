519217680740b8709d32a7f5af329549c4a42e60
suvlife <128946420+suvlife@users.noreply.github.com>
2024-02-29 12:59:34 +0800

    Update chatgpt.ts
    maxtoken

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -44,15 +44,15 @@ export class ChatGPTBot {
     model: "gpt-3.5-turbo-0125",
     // add your ChatGPT model parameters below
     temperature: 0.8,
-    // max_tokens: 128000,
+    max_tokens: 128000,
   };
 
   // ChatGPT system content configuration (guided by OpenAI official document)
   currentDate: string = new Date().toISOString().split("T")[0];
-  chatgptSystemContent: string = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: ${this.currentDate}`;
+  chatgptSystemContent: string = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2024-01-15\nCurrent date: ${this.currentDate}`;
 
   // message size for a single reply by the bot
-  SINGLE_MESSAGE_MAX_SIZE: number = 5000;
+  SINGLE_MESSAGE_MAX_SIZE: number = 50000;
 
   // OpenAI API
   private openaiAccountConfig: any; // OpenAI API key (required) and organization key (optional)
